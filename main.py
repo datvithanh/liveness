9d1e53ee4e5eade500601337e32389cb3a7ab883
datvithanh98 <datvithanh98@gmail.com>
2019-09-06 11:22:59 +0700

    update arg

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from trainer import Trainer, Finetuner
 parser = argparse.ArgumentParser('Training anti-proofing model')
 
 parser.add_argument('--data_path', type=str, help='Path to dataset')
-parser.add_argument('--finetuning', type=bool, help='Finetuning for not')
+parser.add_argument('--finetuning', action='store_true', help='Finetuning for not')
 parser.add_argument('--model_path', type=str, default='', help='Path to trained model')
 param = parser.parse_args()
 
 
 if param.finetuning:
-    trainer = Finetuner(param.data_path, )
+    trainer = Finetuner(param.data_path, param.model_path)
     trainer.load_data()
     trainer.set_model()
     trainer.train()
