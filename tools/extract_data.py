1cf7bd974101f494c3a9bd33eee54799cd5a7879
Thanh Cong Le <thanhlct@gmail.com>
2019-09-09 16:33:56 +0700

    Final data extractor for ROSE

--- tools/extract_data.py
+++ tools/extract_data.py
@@ -5,11 +5,13 @@ import random
 from tqdm import tqdm
 import imutils
 import numpy as np
+import shutil
 from face_detection.face_detection import detect_face_and_rotate, to_rgb
 import pdb
 
 data_dir = '/home/common_gpu0/corpora/vision/liveness/rose/videos/'
 out_dir = '/home/common_gpu0/corpora/vision/liveness/rose/images/'
+#out_dir = '/home/common_gpu0/corpora/vision/liveness/rose/images_not_expanded/'
 miss_dir = '/home/common_gpu0/corpora/vision/liveness/rose/miss/'
 debug = False
 example_duration = 3.0 #second
@@ -92,7 +94,10 @@ def extract_face(detected_frames, box):
     left, top, right, bottom = box
     ret_frames = []
     for frame in detected_frames:
-        ret_frames.append(frame[top:bottom, left:right])
+        if left<0 or top <0:
+            left = max(left, 0)
+            top = max(top, 0)
+        ret_frames.append(frame[top:bottom, left:right, 0:3])
     return ret_frames
 
 def augument_move(box, move_type):
@@ -128,21 +133,47 @@ def read_processed_videos():
         videos = [v.strip() for v in videos]
         return videos
 
+def read_forced_videos():
+    with open(os.path.join(data_dir, 'forced_videos.txt'), 'rt') as f:
+        videos = f.readlines()
+        ret_videos = []
+        for video in videos:
+            idx = video.index('__')
+            video = video[:idx] + '.mp4'
+            video = video.replace('images', 'videos')
+            ret_videos.append(video)
+        return ret_videos
+
 def extract_dir():
     #processed_videos = []
     processed_videos = read_processed_videos()
+    forced_videos = read_forced_videos()
     for sub in glob.glob(os.path.join(data_dir, '*')):
         sub_name = os.path.basename(sub)
-        if sub_name in ['15', '17']:
-            continue
+        #if sub_name in ['15', '17']:#not save in the processed file, if rerun then remove this code
+        #    continue
+        #work_subs = '10  12  13  16  2  21  22'.split()
+        #work_subs = '23  3  4  5  7  9'.split()
+        #if sub_name not in work_subs:
+        #    print('Ignore sub', sub_name)
+        #    continue
+
         print('-'*20, sub_name)
         sub_out_dir = os.path.join(out_dir, sub_name)
         make_dir(sub_out_dir)
         video_count = 0
         for fv in tqdm(glob.glob(os.path.join(sub, '*'))):
-            if fv in processed_videos:
-                print('Processed: ', fv)
-                continue
+            if fv in processed_videos or sub_name in ['15', '17']:
+                if fv not in forced_videos:
+                    print('Processed: ', fv)
+                    continue
+                else:
+                    #delete all examples from the videos
+                    example_dir, _ = os.path.splitext(fv)
+                    example_dir = example_dir.replace('videos', 'images')
+                    example_dirs = glob.glob(example_dir+'*')
+                    for d in example_dirs:
+                        shutil.rmtree(d)    
             fps, frames = read_all_frames(fv)
             num_examples = int(len(frames)/(fps*example_duration))
             #print('{}s\t{}\t{}'.format(round(len(frames)/fps,3), num_examples, fv))
@@ -203,27 +234,61 @@ def extract_dir():
                 write_processed_videos(processed_videos) 
     write_processed_videos(processed_videos)
 
-def correct_gamma(y2, y1):
+def correct_gamma(img, y2, y1):
+    img = np.divide(img, 255.0)
+    pow_rate = y2/y1
+    img = np.power(img, pow_rate)
+    img = img*255.0
+    img = np.trunc(img)
+    img[img>255] = 255
+    img = img.astype('uint8')
+    return img
+
+def del_gamma():
     for sub in glob.glob(os.path.join(out_dir, '*')):
-        for example in glob.glob(os.path.join(sub, '*')):
+        for example in tqdm(glob.glob(os.path.join(sub, '*'))):
+            if '_gam' in example:
+                shutil.rmtree(example)
+                #pass
+                
+def augument_gamma():
+    for sub in glob.glob(os.path.join(out_dir, '*')):
+        #work_subs = '1 2 3 4 5 6 7 8 9 10 11 12'.split()
+        #work_subs = '13 14 15 16 17 18 19 20 21 22 23 34'.split()
+        #sub_name = os.path.basename(sub)
+        #if sub_name not in work_subs:
+        #    print('Ignore sub', sub_name)
+        #    continue
+
+        print('-'*20, sub)
+        examples = glob.glob(os.path.join(sub, '*'))
+        for example in tqdm(examples):
+            if example.endswith('_gam1') or example.endswith('_gam2'):
+                continue
+            gam1_path = '{}_{}'.format(example, 'gam1')
+            gam2_path = '{}_{}'.format(example, 'gam2')
+            if make_dir(gam1_path)==False and make_dir(gam2_path)==False:
+                #print('processed: ', gam1_path)
+                continue
+            make_dir(gam2_path)
+
             for img_path in glob.glob(os.path.join(example, '*')):
-                img = cv2.imread(img_path)
-                img = np.divide(img, 255.0)
-                pow_rate = y2/y1
-                img = np.power(img, pow_rate)
-                img = img*255.0
-                img = np.trunc(img)
-                img[img>255] = 255
-                img = img.astype('uint8')
-                #continue here: create the example directory and push these correct gramma into
+                origin_img = cv2.imread(img_path)
+                if origin_img is None:
+                    print('ERROR', example)
+                    break
+                gam1_img = correct_gamma(origin_img.copy(), 1.0, 2.2)
+                gam2_img = correct_gamma(origin_img.copy(), 2.2, 1.0)
+                img_name = os.path.basename(img_path)
+                write_frame(gam1_img, os.path.join(gam1_path, img_name))
+                write_frame(gam2_img, os.path.join(gam2_path, img_name))
 
 if __name__ == '__main__':
-    extract_dir()
-    #correct_gamma(1.0, 2.2)
-    #correct_gamma(2.2, 1.0)
+    #extract_dir()
+    #del_gamma()
+    augument_gamma()
 
-#TODO:
-# Correct gamut
+#TODO 
 
 #Notice:
 #some example might not have enough 9 faces - no detects found
